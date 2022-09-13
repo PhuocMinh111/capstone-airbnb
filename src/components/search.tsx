ba42cb4fd8ab3ee101b0bcf0661b4f9cb2eb3cc4
PhuocMinh111 <phuocminhlenguyen@gmail.com>
2022-09-13 11:35:03 +0700

    add date picker

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { FaSearch } from "react-icons/fa";
+import BasicDatePicker from "./dataPicker";
+
 const navigation = [
   { name: "Dashboard", href: "#", current: false },
   { name: "Team", href: "#", current: false },
@@ -20,6 +22,28 @@ function classNames(...classes: any[]) {
 function Search() {
   const navigate = useNavigate();
   const [show, setshow] = useState<boolean>(false);
+  const containRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    function outsideClose(e: any) {
+      const dialog = document.querySelector('[role="dialog"]');
+
+      if (
+        containRef.current &&
+        !containRef.current.contains(e.target) &&
+        !dialog?.contains(e.target)
+      ) {
+        console.log(e.target);
+
+        setshow(false);
+      }
+    }
+    document.addEventListener("click", outsideClose);
+
+    return () => {
+      document.removeEventListener("click", outsideClose);
+    };
+  }, [containRef]);
+
   return (
     <Wrapper className="mx-auto lg:w-2/3 lg:h-auto sm:w-25">
       <div
@@ -27,7 +51,7 @@ function Search() {
           show ? "h-16" : "h-0"
         } transition-all sm:mx-auto overflow-hidden sm:block `}
       >
-        <div className="flex space-x-4">
+        <div className="flex mx-auto space-x-4">
           {navigation.map((item, index) => (
             <a
               key={item.name}
@@ -48,26 +72,21 @@ function Search() {
       </div>
 
       {show ? (
-        <form
-          onBlur={() => {
-            setshow(false);
-          }}
-          className={`${
-            show ? "block" : "hidden"
-          } flex h-auto px-4 border-2 bg-light lg:w-auto sm:w-2/3 rounded-full shadow-sm`}
-        >
-          <div className="flex flex-col lg:w-1/4 mx-2 ">
-            <label id="place" htmlFor="place" className="text-lg">
-              dia diem
-            </label>
-            <input id="place" type="text" placeholder="" />
-          </div>
-          <input
-            type="text"
-            className={`${show ? "block" : "hidden"}lg:w-1/4 mx-2`}
-            placeholder=""
-          />
-        </form>
+        <div ref={containRef}>
+          <form
+            className={`${
+              show ? "block" : "hidden"
+            } flex h-auto px-4 border-2 bg-light lg:w-auto sm:w-2/3 rounded-full shadow-sm`}
+          >
+            <div className="flex flex-col lg:w-1/4 mx-2 ">
+              <label id="place" htmlFor="place" className="text-lg">
+                dia diem
+              </label>
+              <input id="place" type="text" placeholder="" />
+            </div>
+            <BasicDatePicker />
+          </form>
+        </div>
       ) : (
         <div
           className="flex h-5 py-4 border-2 justify-center bg-light items-center  sm:w-2/3 rounded-full shadow-sm"
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import BasicDatePicker from "../components/dataPicker";
 import Search from "../components/search";
 
 const Home = () => {
-  return <>Home</>;
+  return (
+    <>
+      Home
+      <BasicDatePicker />
+    </>
+  );
 };
 
 export default Home;
