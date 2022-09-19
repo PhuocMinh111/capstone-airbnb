cf8055305ea07fc05675d5c8c8ca87f64986a68a
phuoc` Minh <duckstuckinbucket@gmail.com>
2022-09-19 03:43:47 +0000

    Update 5 files

--- src/pages/places.tsx
+++ src/pages/places.tsx
@@ -1,28 +1,43 @@
+import { useSelect } from "@mui/base";
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import SinglePlace from "../components/singlePlace";
-import UseIntersectionObserver from "../hooks/useIntersectionObserver";
+import type { RootState } from "../store/store";
 import { FetchPlaceApi } from "../services/place.api";
 import { IPlace } from "../types/interface";
+import styled from "styled-components";
 
 function Places() {
   const [places, setPlaces] = useState<Array<IPlace> | any>();
-
+  const isSearchOpen = useSelector(
+    (state: RootState) => state.navbar.isSearchOpen
+  );
   useEffect(() => {
     fetchPlaceApi();
   }, []);
-
+  //   console.log(canClick);
+  console.log(isSearchOpen);
   const fetchPlaceApi = async () => {
     const result = await FetchPlaceApi();
     setPlaces(result.data.slice(0, 20));
   };
   if (!places) return null;
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 ">
+    <div className="grid relative grid-cols-1 md:grid-cols-4 ">
       {places?.map((item: IPlace, index: any) => {
         return <SinglePlace {...item} key={index} />;
       })}
+      {isSearchOpen && <ClickArea className=" bg-slate-500  "></ClickArea>}
     </div>
   );
 }
-
+const ClickArea = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 25;
+  opacity: 0.3;
+`;
 export default Places;
--- src/store/reducers/navBarReducer.ts
+++ src/store/reducers/navBarReducer.ts
@@ -1,26 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
-
 interface navbarState {
-  isSideBarOpen:boolean;
+  isSideBarOpen: boolean;
+  isSearchOpen: boolean;
 }
 const INITAL_STATE: navbarState = {
-  isSideBarOpen:false
+  isSideBarOpen: false,
+  isSearchOpen: false,
 };
 
 const navBarSlice = createSlice({
   name: "searchForm",
   initialState: INITAL_STATE,
   reducers: {
-   openSidebar:(state):void =>{
-       console.log('open')
-       state.isSideBarOpen = true;
-   },
-   closeSidebar:(state):void =>{
-       state.isSideBarOpen = false;
-   },
+    openSidebar: (state): void => {
+      console.log("open");
+      state.isSideBarOpen = true;
+    },
+
+    closeSidebar: (state): void => {
+      state.isSideBarOpen = false;
+    },
+    openSearch: (state): void => {
+      console.log(state.isSearchOpen);
+      state.isSearchOpen = true;
+    },
+    closeSearch: (state): void => {
+      state.isSearchOpen = false;
+    },
   },
 });
-export const { openSidebar,closeSidebar } = navBarSlice.actions;
+export const { openSearch, closeSearch, openSidebar, closeSidebar } =
+  navBarSlice.actions;
 export default navBarSlice.reducer;
